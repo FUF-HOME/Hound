a998442c0b80a652b66b6345192ad682a1721d15
FUF-HOME <878155981@qq.com>
2019-02-18 18:00:07 +0800

    实现异步爬虫框架

---
+++ hound/spider/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from .Field import AttrField, BaseField, HtmlField, RegexField, TextField
+from .Item import Item
+from .Middleware import Middleware
+from .Request import Request
+from .Response import Response
+
+
