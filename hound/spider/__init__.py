460a499de6561048fd4dabce1a0b292345a05647
FUF-HOME <878155981@qq.com>
2019-02-18 18:03:15 +0800

    Revert "实现异步爬虫框架"
    This reverts commit a998442c0b80a652b66b6345192ad682a1721d15.

--- hound/spider/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-from .Field import AttrField, BaseField, HtmlField, RegexField, TextField
-from .Item import Item
-from .Middleware import Middleware
-from .Request import Request
-from .Response import Response
-
-
